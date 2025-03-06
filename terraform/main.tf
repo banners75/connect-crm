27eef05107c6f339d2527996adecb790e5c8bbcd
Paul Banfield <pmbanfield@hotmail.com>
2025-03-06 22:25:07 +0000

    update bucket name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket"
+    bucket = "banfield-terraform-state-bucket"
     key    = "ecs/connect-crm/terraform.tfstate"
     region = "eu-west-1"
   }
