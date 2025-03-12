499cdf12781f6553909bfebdf1ed5cb66c4d85bf
Paul Banfield <pmbanfield@hotmail.com>
2025-03-12 21:42:01 +0000

    fix test

--- src/tasks/domain/list-all-tasks/listAllTasks.ts
+++ src/tasks/domain/list-all-tasks/listAllTasks.ts
@@ -5,8 +5,8 @@ export class ListAllTasks {
   constructor(private taskRepository: ITaskRepository) {}
 
   async execute(): Promise<Task[]> {
-    // const tasks = this.taskRepository.findAll();
-
-    return new Promise<Task[]>(() => new Array<Task>());
+    const tasks = this.taskRepository.findAll();
+    return tasks;
+    // return new Promise<Task[]>(() => new Array<Task>());
   }
 }
